b99f1819665bf2cd3e81f1aedc994b346cc529bc
Gupta-Kartik7658 <kartik.gupta7658@gmail.com>
2025-05-19 02:07:58 +0530

    Fix API

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string; // Add any other env variables here
+  readonly VITE_API_URL: string;
+  // add more environment variables here as needed, e.g.
+  // readonly VITE_ANOTHER_VAR: string;
 }
 
 interface ImportMeta {
